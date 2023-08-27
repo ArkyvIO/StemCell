2f31eeb1082c8ca3e07ced829a862fc1f691eddc
ArkyvIO <arkyvio@gmail.com>
2023-08-27 17:11:18 -0400

    Added lity and added youtube popup

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,28 @@
 $(document).ready(function () {
+  $("#homebutton").click(function () {
+    $([document.documentElement, document.body]).animate(
+      {
+        scrollTop: $(".hero-container").offset().top,
+      },
+      1000
+    );
+  });
 
-    $('#homebutton').click(function () {
-        $([document.documentElement, document.body]).animate({
-            scrollTop: $(".hero-container").offset().top
-        }, 1000);
-    });
+  $("#infobutton").click(function () {
+    $([document.documentElement, document.body]).animate(
+      {
+        scrollTop: $(".info-container").offset().top,
+      },
+      1000
+    );
+  });
 
-    $('#infobutton').click(function () {
-        $([document.documentElement, document.body]).animate({
-            scrollTop: $(".info-container").offset().top
-        }, 1000);
-    });
-
-    $('#signbutton').click(function () {
-        $([document.documentElement, document.body]).animate({
-            scrollTop: $(".copy-container").offset().top
-        }, 1000);
-    });
-
+  $("#signbutton").click(function () {
+    $([document.documentElement, document.body]).animate(
+      {
+        scrollTop: $(".copy-container").offset().top,
+      },
+      1000
+    );
+  });
 });
